3307d073efa4c51398b3db1be592b2dca8b3286d
Mahmoud Amr Hassan <100859586+mahmoudamr5896@users.noreply.github.com>
2024-12-26 21:08:23 +0200

    Update Payment.js

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -29,7 +29,7 @@ export default function PaymentInfo({ formData, updateFormData, onNext }) {
         formData.append('image', file);
     
         try {
-          const response = await axios.post('http://localhost:3001/upload', formData, {
+          const response = await axios.post('https://backend-taheed.onrender.com/upload', formData, {
             headers: {
               'Content-Type': 'multipart/form-data',
             },
